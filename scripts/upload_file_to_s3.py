c4a7ce7b827b27913f17549eac866324b94009ea
Parthiban D <parthiban.dayalan@gmail.com>
2021-07-07 12:08:56 +0530

    test

--- scripts/upload_file_to_s3.py
+++ scripts/upload_file_to_s3.py
@@ -11,7 +11,7 @@ def main():
         sys.exit(1)
 
     print("Upload to S3 python script initiated")
-    print(    os.system('ls -a target/tickettracker-b-0.0.1-SNAPSHOT.jar'))
+    #print(    os.system('ls -a target/tickettracker-b-0.0.1-SNAPSHOT.jar'))
     bucket_name=sys.argv[1]
     aws_key=sys.argv[2]
     aws_access_key=sys.argv[3]
@@ -25,6 +25,7 @@ def main():
     client = session.client('s3')
 
     os.system('ls -l')
+    os.system('ls -l target')
     os.system('pwd')
 
     #response = client.upload_file(
